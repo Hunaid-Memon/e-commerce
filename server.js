ce095df3da95f2d9cba13999405ef5cbb9ace42e
Hunaid <61511895+Hunaid-Memon@users.noreply.github.com>
2022-03-07 00:29:48 +0500

    Project init and connect from mongodb

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json({ extended: false })); 
+
+const PORT = process.env.PORT || 5004;
+
+app.get("/", (req, res) => {
+    res.status(200).json({ msg: "This is root" });
+})
+
+app.listen(PORT, () => {
+    console.log(`Server has been started\nhttp://localhost:${PORT}`)
+})
