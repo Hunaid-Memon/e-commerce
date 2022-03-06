9edc7ee16004c0237daa2584ca9061bcecaa793d
Hunaid <61511895+Hunaid-Memon@users.noreply.github.com>
2022-03-07 00:52:05 +0500

    Created db.js

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
-
 const app = express();
+const connectDB = require('./config/db')
+
+connectDB();
 
 app.use(express.json({ extended: false })); 
 
 const PORT = process.env.PORT || 5004;
 
 app.get("/", (req, res) => {
-    res.status(200).json({ msg: "This is root" });
-})
+    res.status(200).json({ msg: "This is e-commerce" });
+});
+
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/users', require('./routes/users'));
+app.use('/api/products', require('./routes/products'));
 
 app.listen(PORT, () => {
     console.log(`Server has been started\nhttp://localhost:${PORT}`)
